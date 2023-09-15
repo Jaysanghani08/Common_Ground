a2875ac7d8242260dd55e71faf748fe72b9eeb0c
Jay Sabva <202101224@daiict.ac.in>
2023-09-15 10:49:35 +0530

    Setup BoilerPlate Code

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
 
 const app = express();
+dotenv.config({path: './config.env'});
+const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: "test",
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
