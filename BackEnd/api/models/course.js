6355650cd3e963e4a5598322327d5d5f2a734ac1
P-a-r-a-m <94957160+P-a-r-a-m@users.noreply.github.com>
2023-09-20 18:55:33 +0530

    Add Course Schema

---
+++ BackEnd/api/models/course.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+  courseTitle: {
+    type: String,
+    required: true,
+    trim: true,
+    Number:false
+  },
+  courseDescription: {
+    type: String,
+    max: 50,
+    required: true
+  },
+  courseDescriptionLong: {
+    type: String,
+    min: 50,
+    required: true
+  },
+  coursePrice: {
+    type: Number,
+    default : 0,
+  },
+  thumbnail: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: [String],
+    required: true
+  },
+  courseLevel: {
+    type: String, // You can specify 'beginner', 'intermediate', 'advanced', etc.
+    required: true,
+    trim: true
+  },
+  courseCode: {
+    type: String,
+    unique: true, 
+    required: true,
+    match: /^[a-zA-Z]+[0-9]+$/,
+  },
+  dateCreated: {
+    type: Date,   //don't take any input
+    default: Date.now,
+  },
+  courseSections: {
+    type: [mongoose.Schema.Types.ObjectId], 
+    ref: 'Section',
+  },
+  courseAssignments: {
+    type: [mongoose.Schema.Types.ObjectId], 
+    ref: 'Assignment',
+  },
+  prerequisites: {
+    type: [string], 
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Educator',
+  },
+  enrolledStudents: {
+    type: [mongoose.Schema.Types.ObjectId], 
+    ref: 'Student',
+  },
+  rating: {
+    type: Number,
+    default: 0, 
+  },
+  discussionForum: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'DiscussionForum',
+  },
+  courseCertificate: {
+    type: String, 
+  },
+});
+
+module.exports = mongoose.model('Course', courseSchema);
