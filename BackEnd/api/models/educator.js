da44925b82d8c91988dcbcc937695680f259b305
P-a-r-a-m <94957160+P-a-r-a-m@users.noreply.github.com>
2023-09-20 14:07:52 +0530

    Edited Educator Schema

--- BackEnd/api/models/educator.js
+++ BackEnd/api/models/educator.js
@@ -4,51 +4,70 @@ const educatorSchema = new mongoose.Schema({
     fname: {
         type: String,
         required: true,
+        trim : true
     },
     lname: {
         type: String,
         required: true,
+        trim : true
     },
     gender: {
         type: String,
         required: true,
+        trim : true
+    },
+    location : {
+        type : String,
+        required : true,
+        trim : true
     },
     dob: {
         type: Date,
-        required: true,
+        required: true
     },
     username: {
         type: String,
         unique: true,
         required: true,
+        trim : true
     },
     password: {
         type: String,
-        required: true,
+        required: true
+        
     },
     phone: {
         type: String,
         required: true,
+        match : "/^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/",
+        trim : true
     },
     email: {
         type: String,
         unique: true,
         required: true,
+        match: "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
     },
     upiID: {
         type: String,
         required: true,
+        match : "^[a-zA-Z0-9.-]{2, 256}@[a-zA-Z][a-zA-Z]{2, 64}$"
     },
     bio: {
         type: String,
+        required : true
     },
     profilePic: {
         type: String,
+        data: BinData(0, "base64_encoded_image_data"),
+        contentType: "image/jpeg",
+        default : "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
+
     },
     courseCreated: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Course',
+            ref: 'Course'
         },
     ],
 });
