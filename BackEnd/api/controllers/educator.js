af51dc40ef3c437b8613582decc5155cd6c9baac
Jay Sabva <202101224@daiict.ac.in>
2023-09-20 14:26:09 +0530

    Add Validation

--- BackEnd/api/controllers/educator.js
+++ BackEnd/api/controllers/educator.js
@@ -1,4 +1,72 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+
+const Educator = require('../models/educator');
+const Student = require("../models/student");
+const Token = require("../models/token");
+
+
+exports.userSignup = async (req, res, next) => {
+    try {
+        let user = await Educator.findOne({email: req.body.email}).exec();
+
+        if (user) {
+            return res.status(409).json({
+                message: 'Mail is already in use - Educator'
+            });
+        }
+
+        user = await Educator.findOne({username: req.body.username}).exec();
+
+        if (user) {
+            return res.status(409).json({
+                message: 'Username is already in use - Educator'
+            });
+        }
+
+        user = await Educator.findOne({phone: req.body.phone}).exec();
+
+        if (user) {
+            return res.status(409).json({
+                message: 'Phone number is already in use - Educator'
+            });
+        }
+
+        const hash = await bcrypt.hash(req.body.password, 10);
+
+        const newUser = new Educator({
+            fname: req.body.fname,
+            lname: req.body.lname,
+            gender: req.body.gender,
+            dob: req.body.dob,
+            location: req.body.location,
+            username: req.body.username,
+            password: hash,
+            phone: req.body.phone,
+            email: req.body.email,
+            upiID: req.body.upiID,
+            bio: req.body.bio,
+            profilePic: req.body.profilePic,
+            courseCreated: req.body.courseCreated,
+        });
+
+        const result = await newUser.save();
+
+        console.log(result);
+        res.status(201).json({
+            message: 'Educator created'
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+};
+
 
 exports.userLogin = async (req, res, next) => {
     try {
