41331042cc32d16686206457e2cf97fb6352ca6b
Jay Sabva <202101224@daiict.ac.in>
2023-09-22 09:59:16 +0530

    Added Section Controller

--- BackEnd/api/controllers/section.js
+++ BackEnd/api/controllers/section.js
@@ -1,18 +1,44 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
 const Section = require('../models/section');
 const Educator = require('../models/educator');
-const jwt = require("jsonwebtoken");
+const Course = require('../models/course');
+
 
 exports.createSection = async (req,res,next) => {
     try {
         req.userData = await jwt.verify(req.headers.authorization.split(" ")[1], process.env.JWT_KEY);
+        const course = await Course.findById(req.params.courseId).exec();
+        if (!course) {
+            return res.status(404).json({
+                message: 'Course not found'
+            });
+        }
+        console.log(course.createdBy);
+        console.log(req.userData.userId);
+        if (course.createdBy.toString() !== req.userData.userId.toString()) {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            });
+        }
 
-        const newSection = new Section ( {
+        const post = {
             title: req.body.title,
-            description: req.body.description,
-            attachments: req.body.attachments,
+            body: req.body.body,
+            attachments: req.body.attachments
+        };
+        const section = new Section({
+            title: req.body.sectionTitle,
+            posts: post
         });
-        await newSection.save();
+
+        await section.save();
+        course.courseSections.push(section._id);
+        await course.save();
+
         res.status(201).json({
             message: 'Section created'
         });
@@ -21,7 +47,132 @@ exports.createSection = async (req,res,next) => {
         (err) {
         console.log(err);
         res.status(500).json({
-            message : 'error while creating',
+            error: err
+        });
+    }
+};
+
+exports.editSection = async (req,res,next) => {
+    try {
+        req.userData = await jwt.verify(req.headers.authorization.split(" ")[1], process.env.JWT_KEY);
+
+        const course = await Course.findById(req.params.courseId).exec();
+        if (!course) {
+            return res.status(404).json({
+                message: 'Course not found'
+            });
+        }
+
+        if (course.createdBy.toString() !== req.userData.userId.toString()) {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            });
+        }
+
+        const section = await Section.findById(req.params.sectionId).exec();
+        if (!section) {
+            return res.status(404).json({
+                message: 'Section not found'
+            });
+        }
+
+        const updateData = req.body;
+
+        await Section.updateOne({_id: req.params.sectionId}, {$set: updateData}).exec();
+
+        res.status(201).json({
+            message: 'Section edited'
+        });
+    }
+    catch
+        (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+}
+
+exports.deleteSection = async (req,res,next) => {
+    try {
+        req.userData = await jwt.verify(req.headers.authorization.split(" ")[1], process.env.JWT_KEY);
+
+        const course = await Course.findById(req.params.courseId).exec();
+        if (!course) {
+            return res.status(404).json({
+                message: 'Course not found'
+            });
+        }
+
+        if (course.createdBy.toString() !== req.userData.userId.toString()) {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            });
+        }
+
+        const section = await Section.findById(req.params.sectionId).exec();
+        if (!section) {
+            return res.status(404).json({
+                message: 'Section not found'
+            });
+        }
+        await Course.updateOne({_id: req.params.courseId}, {$pull: {courseSections: req.params.sectionId}}).exec();
+
+        await Section.deleteOne({_id: req.params.sectionId}).exec();
+
+        res.status(201).json({
+            message: 'Section deleted'
+        });
+    }
+    catch
+        (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+}
+exports.addPost = async (req,res,next) => {
+    try {
+        req.userData = await jwt.verify(req.headers.authorization.split(" ")[1], process.env.JWT_KEY);
+        const course = await Course.findById(req.params.courseId).exec();
+
+        if (!course) {
+            return res.status(404).json({
+                message: 'Course not found'
+            });
+        }
+
+        if (course.createdBy.toString() !== req.userData.userId.toString()) {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            });
+        }
+
+        const section = await Section.findById(req.params.sectionId).exec();
+        if (!section) {
+            return res.status(404).json({
+                message: 'Section not found'
+            });
+        }
+
+        const post = {
+            title: req.body.title,
+            body: req.body.body,
+            attachments: req.body.attachments
+        };
+
+        section.posts.push(post);
+        await section.save();
+
+        return res.status(201).json({
+            message: 'Post added'
+        });
+    }
+    catch
+        (err) {
+        console.log(err);
+        res.status(500).json({
             error: err
         });
     }
