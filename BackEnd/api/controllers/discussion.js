a948dde5f449291eab157e8166cd46a77e6bd66d
Jay Sabva <202101224@daiict.ac.in>
2023-09-22 12:10:30 +0530

    Added All Method in discussion.js

--- BackEnd/api/controllers/discussion.js
+++ BackEnd/api/controllers/discussion.js
@@ -48,3 +48,101 @@ exports.addMessage = async (req, res, next) => {
         });
     }
 };
+
+exports.editMessage = async (req, res, next) => {
+    try {
+        req.userData = await jwt.verify(req.headers.authorization.split(" ")[1], process.env.JWT_KEY);
+
+        const course = await Course.findById(req.params.courseId).exec();
+
+        if (!course) {
+            return res.status(404).json({
+                message: 'Course not found'
+            });
+        }
+
+        if (!course.discussionForum) {
+            return res.status(404).json({
+                message: 'This course does not have a discussion forum'
+            });
+        }
+
+        const discussionId = course.discussionForum;
+        const discussion = await Discussion.findById(discussionId).exec();
+
+        const message = discussion.messages.id(req.params.messageId);
+
+        if (!message) {
+            return res.status(404).json({
+                message: 'Message not found'
+            });
+        }
+
+        if (message.createdByStudent.toString() !== req.userData.userId.toString()) {
+            return res.status(403).json({
+                message: 'You are not authorized to edit this message'
+            });
+        }
+
+        message.message = req.body.message;
+        await discussion.save();
+
+        res.status(200).json({
+            message: 'Message edited'
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+};
+
+exports.deleteMessage = async (req, res, next) => {
+    try {
+        req.userData = await jwt.verify(req.headers.authorization.split(" ")[1], process.env.JWT_KEY);
+
+        const course = await Course.findById(req.params.courseId).exec();
+
+        if (!course) {
+            return res.status(404).json({
+                message: 'Course not found'
+            });
+        }
+
+        if (!course.discussionForum) {
+            return res.status(404).json({
+                message: 'This course does not have a discussion forum'
+            });
+        }
+
+        const discussionId = course.discussionForum;
+        const discussion = await Discussion.findById(discussionId).exec();
+
+        const message = discussion.messages.id(req.params.messageId);
+
+        if (!message) {
+            return res.status(404).json({
+                message: 'Message not found'
+            });
+        }
+
+        if (message.createdByStudent.toString() !== req.userData.userId.toString()) {
+            return res.status(403).json({
+                message: 'You are not authorized to delete this message'
+            });
+        }
+
+        await discussion.messages.pull(req.params.messageId);
+        await discussion.save();
+
+        res.status(200).json({
+            message: 'Message deleted'
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+};
