7c730c9f7e1cd1ad9d1ca580c4410fd90e319056
Jay Sabva <202101224@daiict.ac.in>
2023-09-22 21:17:33 +0530

    Added Profile Photo Upload Feature in student.js

--- BackEnd/api/routes/student.js
+++ BackEnd/api/routes/student.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { profilePhoto } = require("../middleware/multerProfile");
 
 const StudentController = require("../controllers/student");
 const CourseController = require("../controllers/course");
@@ -8,7 +9,7 @@ const DiscussionController = require("../controllers/discussion");
 const checkAuth = require("../middleware/checkAuth");
 const checkEnroll = require("../middleware/checkEnroll");
 
-router.post("/signup", StudentController.userSignup);
+router.post("/signup", profilePhoto.single('profilePic'), StudentController.userSignup);
 router.post("/login", StudentController.userLogin);
 router.post('/reset-password', StudentController.resetPassword);
 router.post('/update-password', StudentController.updatePassword);
@@ -17,10 +18,9 @@ router.delete("/:email", checkAuth, StudentController.userDelete);
 router.post("/enroll/:courseId", checkAuth, CourseController.enrollCourse);
 router.post("/unenroll/:courseId", checkAuth, CourseController.unenrollCourse);
 
-
-// under progress
 router.post("/:courseId/discussion", checkAuth, checkEnroll, DiscussionController.addMessage);
 router.patch("/:courseId/discussion/:messageId", checkAuth, checkEnroll, DiscussionController.editMessage);
 router.delete("/:courseId/discussion/:messageId", checkAuth, checkEnroll, DiscussionController.deleteMessage);
+
 
 module.exports = router;
