58f089c71b7ea400ff50c0837325977fe66f6036
Jay Sabva <202101224@daiict.ac.in>
2023-09-23 21:13:49 +0530

    Fix Naming of file in multerCourse.js.js

--- BackEnd/api/middleware/multerCourse.js
+++ BackEnd/api/middleware/multerCourse.js
@@ -48,7 +48,7 @@ const storage = multerProfile.diskStorage({
         }
     },
     filename: function (req, file, cb) {
-        cb(null, req.body.courseTitle + Date.now() + file.originalname);
+        cb(null, req.body.courseTitle + '-' + 'Thumbnail' + '-' + file.originalname);
     }
 });
 
