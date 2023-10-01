37532057b9d60dbce193f8d74c06093021420a4c
Ankur Damor <ankurdamor1110@gmail.com>
2023-10-01 23:04:21 +0530

    Added some functions for Responsiveness

--- src/pages/Educator/Dashboard/Graph/Graph.js
+++ src/pages/Educator/Dashboard/Graph/Graph.js
@@ -1,38 +1,3 @@
-// import * as React from 'react';
-// import { BarChart } from '@mui/x-charts/BarChart';
-// import './Graph.css';
-
-
-// const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490, 3766, 2113, 3213, 1232];
-// const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300, 4000, 3000, 2000, 2780];
-// const xLabels = [
-//   'A',
-//   'B',
-//   'C',
-//   'D',
-//   'E',
-//   'F',
-//   'G',
-//   'H',
-//   'I',
-//   'J',
-//   'K'
-// ];
-
-// export default function SimpleBarChart() {
-//   return (
-//     <BarChart
-//       width={800}
-//       height={300}
-//       series={[
-//         { data: pData, label: 'pv', color:"#4f39bf" },
-//         { data: uData, label: 'uv', color:"#cf6624"},
-//       ]}
-//       xAxis={[{ scaleType: 'band', data: xLabels }]}
-//     />
-//   );
-// }
-
 import React, { useEffect } from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
 import './Graph.css';
@@ -55,7 +20,7 @@ const xLabels = [
 
 export default function SimpleBarChart() {
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    const mediaQuery = window.matchMedia('(max-width: 600px)');
 
     function handleMediaQueryChange(event) {
       // When the media query condition is met, update the width to 'auto'
