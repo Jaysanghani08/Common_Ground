7b4fb0615668d4918d5025ccb5b6b4b8ddefee12
Jaysanghani08 <sanghanijay36@gmail.com>
2023-10-26 15:24:59 +0530

    Pages Created in Edu Folder

---
+++ src/pages/Educator/CreateCourse/EduCreateCourse.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EduCreateCourse = () => {
+    return (
+        <div>
+            Edu-Create Curse page
+        </div>
+    )
+}
+
+export default EduCreateCourse
---
+++ src/pages/Educator/EduOfferedCourses/EduOfferedCourses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EduOfferedCourses = () => {
+    return (
+        <div>
+            EduOffered Course Page
+        </div>
+    )
+}
+
+export default EduOfferedCourses
