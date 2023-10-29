50fedfb3ead4638f2d2ea4ac9f47f97d911f9b80
Ankur Damor <ankurdamor1110@gmail.com>
2023-10-30 00:32:14 +0530

    Added accordion for the student course page

--- src/pages/CourseDetail/CourseDetailComponents/CourseHeader/TabMenu.js
+++ src/pages/CourseDetail/CourseDetailComponents/CourseHeader/TabMenu.js
@@ -4,6 +4,52 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import IconButton from '@mui/material/IconButton'; 
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf'; 
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import './TabMenu.css';
+
+function CustomAccordion(props) {
+    const { title, pdfLink, assignmentLink, details } = props;
+
+    return (
+        <Accordion style={{ margin: '10px 0' }}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color="primary">{title}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+            <div>
+                    <h5>{details}</h5>
+                </div>
+                <ul>
+                    <li>
+                        <IconButton>
+                            <PictureAsPdfIcon style={{fontSize:"25px"}} />
+                        </IconButton>
+                        <a href={pdfLink}>PDF Material</a>
+                    </li>
+                    <li>
+                        <IconButton>
+                            <AssignmentIcon style={{fontSize:"25px"}} />
+                        </IconButton>
+                        <a href={assignmentLink}>Assignment</a>
+                    </li>
+                </ul>
+            </AccordionDetails>
+        </Accordion>
+    );
+}
+
+CustomAccordion.propTypes = {
+    title: PropTypes.string.isRequired,
+    pdfLink: PropTypes.string.isRequired,
+    assignmentLink: PropTypes.string.isRequired,
+    details: PropTypes.string.isRequired,
+};
 
 function CustomTabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -18,7 +64,27 @@ function CustomTabPanel(props) {
         >
             {value === index && (
                 <Box sx={{ p: 3 }}>
-                    <Typography color={"gray"}>childre</Typography>
+                    <Typography color={"gray"}>
+                        <CustomAccordion
+                            title="Week 1"
+                            pdfLink="link-to-pdf-1"
+                            assignmentLink="link-to-assignment-1"
+                            details="This is the details for Week 1."
+                        />
+                        <CustomAccordion
+                            title="Week 2"
+                            pdfLink="link-to-pdf-2"
+                            assignmentLink="link-to-assignment-2"
+                            details="This is the details for Week 2."
+                        />
+                        <CustomAccordion
+                            title="Week 3"
+                            pdfLink="link-to-pdf-3"
+                            assignmentLink="link-to-assignment-3"
+                            details="This is the details for Week 3."
+                        />
+                        {/* Add more accordions here */}
+                    </Typography>
                 </Box>
             )}
         </div>
