a9b63c6257dce6eac1871fed3156f67e0c828db0
Jay Sabva <202101224@daiict.ac.in>
2023-11-03 21:25:34 +0530

    Added General API Route

--- BackEnd/api/routes/query.js
+++ BackEnd/api/routes/query.js
@@ -4,14 +4,19 @@ const router = express.Router();
 const QueryController = require('../controllers/query');
 
 const checkAuth = require('../middleware/checkAuth');
+const checkEnroll = require('../middleware/checkEnroll');
 
 router.get('/allcourse', QueryController.getAllCourse);
 router.get('/course', QueryController.getCourse);
-router.get('/coursebyeducator', QueryController.getCourseByEducator);
-router.get('/enrolledcourse', QueryController.getEnrolledCourse);
+router.get('/coursebyeducator', checkAuth, QueryController.getCourseByEducator);
 // router.get('/searchcourse', QueryController.searchCourse);
 router.get('/search-filter', QueryController.searchFilter);
 
 
 router.get('/dashboard', checkAuth, QueryController.getDashboard);
+router.get('/profile', checkAuth, QueryController.getProfile);
+router.get('/getCourses', QueryController.getCourses);
+router.get('/enrolled-course', checkAuth, QueryController.getEnrolledCourse);
+router.get('/getCourse', checkAuth, checkEnroll, QueryController.getCourse);
+
 module.exports = router;
