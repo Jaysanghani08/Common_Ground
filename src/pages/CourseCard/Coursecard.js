73d0a1b2fe8e778bd8d7380e4c7f4193807c66fd
Jaysanghani08 <sanghanijay36@gmail.com>
2023-11-03 13:40:31 +0530

    something

--- src/pages/CourseCard/Coursecard.js
+++ src/pages/CourseCard/Coursecard.js
@@ -36,7 +36,7 @@ const Coursecard = ({ course }) => {
                             <h4>Progress</h4>
                         </span>
                         <span className="courses_product-color">
-                            <h4>{course.enrolledStudents.length} students enrolled</h4>
+                            <h4>{course.enrolledStudents ? course.enrolledStudents.length : 0} students enrolled</h4>
                         </span>
                         <span className="courses_product-price">
                             <span> {course.coursePrice === 0 ? "Free" : `Rs. ${course.price}`} </span>
