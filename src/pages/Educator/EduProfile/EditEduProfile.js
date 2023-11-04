b390fd9bc86854ed57a7a17c3c9dfe7fc1031e68
Sahilmachhar <141858272+Sahilmachhar@users.noreply.github.com>
2023-11-04 23:43:37 +0530

    Update EditEduProfile.js

--- src/pages/Educator/EduProfile/EditEduProfile.js
+++ src/pages/Educator/EduProfile/EditEduProfile.js
@@ -2,81 +2,129 @@ import React from 'react';
 import './EditEduProfile.css';
 
 function EditEduProfile() {
-  return (
-    <div className="ep-main-container">
-      <h1 className="ep-heading">Edit Profile</h1>
-      
-      <div className="ep-sub-main">
-        <div className="ep-photo-edit">
-          {/* <div className="text-center"> */}
-            <img
-              src="https://img.freepik.com/premium-vector/man-character_665280-46970.jpg"
-              className="avatar img-circle img-thumbnail"
-              alt="avatar"
-            />
-            <h6>Upload a different photo...</h6>
-            <input type="file" className="ep-form-control" />
-          {/* </div> */}
-        </div>
 
-        <div className="ep-personal-info">
-          
-          <h3>Personal info</h3>
-          <form >
-            <div className="ep-form-group">
-              <label >Your Name :</label>
-              <div className="">
-                <input className="ep-form-control" type="text" value="Sahil" />
+  return (
+
+      <div className="edp-main-container">
+         <h1 className="edp-heading">Edit Profile</h1>
+         <hr/>
+
+            <form >
+              <div className="edp-sub-main">
+
+                  <div className='edp-first-form'>
+
+                    <h3>Personal info : </h3>
+
+                    <div className="edp-form-group">
+                            <label htmlFor='username'>Username :</label>
+                            <div className="">
+                              <input className="edp-form-control" type="text" name='username' value="Edit_Profile" />
+                            </div>
+                          </div>
+
+                    <div className="edp-personal-info">
+
+                        <div className='edp-first-col'>
+
+                          <div className="edp-form-group">
+                            <label htmlFor='fname'>First Name :</label>
+                            <div className="">
+                              <input className="edp-form-control" type="text" name='fname' value="Sahil" />
+                            </div>
+                          </div>
+
+                          <div className="edp-form-group">
+                            <label htmlFor='gender'>Gender :</label>
+                            <div className="">
+                              <input className="edp-form-control" type="text" name='gender' value="Male" />
+                            </div>
+                          </div>
+
+                          <div className="edp-form-group">
+                            <label htmlFor='country'>Country :</label>
+                            <div className="">
+                              <input className="edp-form-control" type="text" name='country' value="India" />
+                            </div>
+                          </div>
+
+                          <div className="edp-form-group">
+                            <label htmlFor='educationLevel'>Education Level :</label>
+                            <div className="">
+                              <input className="edp-form-control" type="text" name='educationLevel' value="" />
+                            </div>
+                          </div>
+
+                        </div>
+
+                        <div className='edp-second-col'>
+
+                          <div className="edp-form-group">
+                            <label htmlFor='lname'>Last Name :</label>
+                            <div className="">
+                              <input className="edp-form-control" type="text" name='lname' value="Machhar" />
+                            </div>
+                          </div>
+
+                          <div className="edp-form-group">
+                            <label htmlFor='dob'>DOB :</label>
+                            <div className="">
+                              <input className="edp-form-control" type="date" name='dob' value="" />
+                            </div>
+                          </div>
+
+                          <div className="edp-form-group">
+                            <label htmlFor='email'>Email :</label>
+                            <div className="">
+                              <input className="edp-form-control" type="email" name='email' value="editform@gmail.com" />
+                            </div>
+                          </div>
+
+                          <div className="edp-form-group">
+                            <label htmlFor='phone'>Mobile number :</label>
+                            <div className="">
+                              <input className="edp-form-control" type="string" name='phone' value="1234567891" />
+                            </div>
+                          </div>
+
+                        </div>
+
+                      </div>
+
+                  </div>
+
+                  <div className='edp-sec-form'>
+
+                    <div className="edp-photo-edit">
+                    
+                      <img
+                        src="https://img.freepik.com/premium-vector/man-character_665280-46970.jpg"
+                        className="edp-avatar"
+                        alt="avatar"
+                      />
+                      <h6>Upload a different photo...</h6>
+                      <input type="file" className="edp-form-control" />
+
+                      <div className="edp-form-group">
+                        <label htmlFor='bio'>About Yourself :</label>
+                        <div className="edp-about">
+                          <textarea className="edp-form-control" name="bio" placeholder="Write a brief bio about yourself..." rows="4" cols="50"></textarea>
+                          <input className="edp-form-control" type="submit"value="Save" />
+                        </div>
+                      </div>
+
+                    </div>
+
+                  </div>
+                  
               </div>
-            </div>
-            <div className="ep-form-group">
-              <label >Country :</label>
-              <div className="">
-                <input className="ep-form-control" type="text" value="India" />
-              </div>
-            </div>
-            <div className="ep-form-group">
-              <label >Age :</label>
-              <div className="">
-                <input className="ep-form-control" type="text" value="20" />
-              </div>
-            </div>
-            <div className="ep-form-group">
-              <label >Degree :</label>
-              <div className="">
-                <input className="ep-form-control" type="text" value="" />
-              </div>
-            </div>
-            <div className="ep-form-group">
-              <label >Field :</label>
-              <div className="">
-                <input className="ep-form-control" type="text" value="" />
-              </div>
-            </div>
-            <div className="ep-form-group">
-              <label >Email :</label>
-              <div className="">
-                <input className="ep-form-control" type="text" value="editform@gmail.com" />
-              </div>
-            </div>
-            <div className="ep-form-group">
-              <label >Mobile number :</label>
-              <div className="">
-                <input className="ep-form-control" type="text" value="1234567891" />
-              </div>
-            </div>
-            <div className="ep-form-group">
-              <label >About Yourself :</label>
-              <div className="ep-about">
-                <textarea className="ep-form-control" name="bio" placeholder="Write a brief bio about yourself..." rows="4" cols="50"></textarea>
-                <input className="ep-form-control" type="submit"value="Save" />
-              </div>
-            </div>
-          </form>
-            <button className='ep-submit-button'>Submit</button>
-        </div>
+
+                  <button className='edp-submit-button'>Submit</button>
+
+            </form>
+
       </div>
-    </div>
+
   );
 }
 
