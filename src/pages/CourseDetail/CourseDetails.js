4405775bfc7c70be55dc2e0c054b895a321ed9b0
Anand202526 <141856406+Anand202526@users.noreply.github.com>
2023-11-04 20:46:30 +0530

    Update CourseDetails.js

--- src/pages/CourseDetail/CourseDetails.js
+++ src/pages/CourseDetail/CourseDetails.js
@@ -4,6 +4,7 @@ import "./CourseDetails.css"
 import CourseHeader from './CourseDetailComponents/CourseHeader/CourseHeader'
 import BasicTabs from './CourseDetailComponents/CourseHeader/TabMenu'
 import DicussionForum from './CourseDetailComponents/DicussionForum/DicussionForum'
+import BasicTextFields from './CourseDetailComponents/Buttons/button'
 
 
 const CourseDetails = () => {
@@ -14,6 +15,7 @@ const CourseDetails = () => {
                 <CourseHeader />
                 <div className="course-content">
                     <BasicTabs />
+                    <BasicTextFields/>
                     <DicussionForum/>
                 </div>
                 
