526aa48c1a5d059cc09182669335739b78caca91
Jaysanghani08 <sanghanijay36@gmail.com>
2023-11-05 12:18:42 +0530

    something

--- src/pages/student/EnrolledCourses/EnrolledCourses.js
+++ src/pages/student/EnrolledCourses/EnrolledCourses.js
@@ -5,7 +5,18 @@ const EnrolledCourses = () => {
     return (
         <div>
             <Navbar />
-            
+            {/* <video src='http://localhost:8000/Campus_Diaries_S1_Ep1.mp4' style={{ margin: "80px" }}> </video> */}
+
+            <video
+                allow="fullscreen"
+                frameBorder="0"
+                width="100%"
+                height="700"
+                controls
+                controlsList="nodownload"
+            >
+                <source src="http://localhost:8000/Campus_Diaries_S1_Ep1.mp4" />
+            </video>
         </div>
     )
 }
