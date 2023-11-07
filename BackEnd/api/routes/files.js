dd825367e9f9cacca0eaee82db5577f7d1f7a15e
Jay Sabva <202101224@daiict.ac.in>
2023-11-08 02:14:09 +0530

    Created FileRoute

---
+++ BackEnd/api/routes/files.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const FileController = require("../controllers/fileStream");
+
+const checkAuth = require("../middleware/checkAuth");
+const checkEnroll = require("../middleware/checkEnroll");
+
+router.get("/video/:courseId/:sectionId/:postId/:fileName", FileController.streamFile);
+router.get("/retrieve", checkAuth, FileController.testStream);
+module.exports = router;
