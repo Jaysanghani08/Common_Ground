914727e47ae396493eb18d3e1356e5d32dd9838f
Chaxupatel <94862042+Chaxupatel@users.noreply.github.com>
2023-11-08 22:55:00 +0530

    Update EduCreateCourse.js

--- src/pages/Educator/CreateCourse/EduCreateCourse.js
+++ src/pages/Educator/CreateCourse/EduCreateCourse.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './CreateCourseForm.css';
 import Sidebar from '../Dashboard/Sidebar/Sidebar'
-// import image from './ss.jpg';
+// import image from './123.jpg';
 import { ToastContainer, toast } from 'react-toastify'
 import { educreatecoursefunction } from '../../../services/Apis';
 import getToken from '../../../services/getToken';
@@ -20,7 +20,7 @@ function EduCreateCourse() {
         thumbnail: '',
         tags: ''
     });
-    
+
     const [courseLevel, setCourseLevel] = useState('');
     const [courseLanguage, setCourseLanguage] = useState('');
     const [visibility, setVisibility] = useState('');
@@ -40,7 +40,7 @@ function EduCreateCourse() {
 
         setInputdata({
             ...inputdata,
-            "thumbnail" : file
+            "thumbnail": file
         })
     }
 
@@ -59,10 +59,10 @@ function EduCreateCourse() {
         const formData = {
             ...inputdata,
             courseLevel,
-            tags : inputdata.tags.split(','),
+            tags: inputdata.tags.split(','),
             // language:"English",
-            prerequisites : inputdata.prerequisites.split(','),
-            language : courseLanguage,
+            prerequisites: inputdata.prerequisites.split(','),
+            language: courseLanguage,
             visibility,
         }
 
@@ -112,152 +112,176 @@ function EduCreateCourse() {
 
     return (
         <>
+
             <div className="container1">
-                <EdNavbar/>
-                <div className="create-course-container maindash">
-                    <form className='ccform' encType='multipart/form-data'>
-                        <div className=".create-course-form">
-                            <h1 style={{ color: "#f35e3d" }}>Create Course</h1>
-                            <div className="form-group">
-                                <label>Course Thumbnail</label>
-                                <input
-                                    type="file"
-                                    name="thumbnail"
-                                    accept='.png'
-                                    onChange={handleFileUpload}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label>Course Code</label>
-                                <input
-                                    type="text"
-                                    name="courseCode"
-                                    value={inputdata.courseCode}
-                                    onChange={handleChange}
-                                    placeholder="Course Code"
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label>Course Title</label>
-                                <input
-                                    type="text"
-                                    name="courseTitle"
-                                    value={inputdata.courseTitle}
-                                    onChange={handleChange}
-                                    placeholder="Course Name"
-                                />
-                            </div>
 
-                            <div className="form-group">
-                                <label>Short Description</label>
-                                <input
-                                    type="text"
-                                    className="description"
-                                    value={inputdata.courseDescription}
-                                    onChange={handleChange}
-                                    placeholder="Enter Your Course Description-short"
-                                    name="courseDescription"
-                                />
+                <EdNavbar />
+                <div className="create-course-container">
+                    <form className='course-form' encType='multipart/form-data'>
+                        <h1 style={{ color: "0C356A" }}>Create A New Course</h1>
+                        <div>
+                            <div className='ccform'>
+
+                                <div className='form-pair'>
+                                    <div className="form-group">
+                                        <label>Course Title</label>
+                                        <input
+                                            type="text"
+                                            name="courseTitle"
+                                            value={inputdata.courseTitle}
+                                            onChange={handleChange}
+                                            placeholder="Course Name"
+                                        />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label>Course Code</label>
+                                        <input
+                                            type="text"
+                                            name="courseCode"
+                                            value={inputdata.courseCode}
+                                            onChange={handleChange}
+                                            placeholder="Course Code"
+                                        />
+                                    </div>
+                                </div>
+                                <div className='form-pair'>
+                                    <div className="form-group">
+                                        <label>Visibility</label>
+                                        <select
+                                            name='visibility'
+                                            value={visibility}
+                                            onChange={(e) => setVisibility(e.target.value)}
+                                        >
+                                            <option value="">Select</option>
+                                            <option value="Public">Public</option>
+                                            <option value="Protected">Protected</option>
+                                            <option value="Private">Private</option>
+                                        </select>
+                                    </div>
+
+
+                                    <div className="form-group">
+                                        <label>Short Description</label>
+                                        <input
+                                            type="text"
+                                            className="description"
+                                            value={inputdata.courseDescription}
+                                            onChange={handleChange}
+                                            placeholder="Enter Your Course Description-short"
+                                            name="courseDescription"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className='form-pair'>
+                                    <div className="form-group">
+                                        <label>Course Level</label>
+                                        <select
+                                            name='courseLevel'
+                                            value={courseLevel}
+                                            onChange={(e) => setCourseLevel(e.target.value)}
+                                        >
+                                            <option value="">Select</option>
+                                            <option value="beginner">Beginner</option>
+                                            <option value="intermediate">Intermediate</option>
+                                            <option value="advanced">Advanced</option>
+                                        </select>
+                                    </div>
+
+
+                                    <div className="form-group">
+                                        <label>Course Price (Rs)</label>
+                                        <input
+                                            type="number"
+                                            value={inputdata.coursePrice}
+                                            onChange={handleChange}
+                                            placeholder="Enter Price (Enter '0' for free)"
+                                            name="coursePrice"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className='form-pair'>
+                                    <div className="form-group">
+                                        <label>Prerequisites</label>
+                                        <input
+                                            name='prerequisites'
+                                            type="text"
+                                            value={inputdata.prerequisites}
+                                            onChange={handleChange}
+                                            placeholder="Enter Course Prerequisites"
+                                        />
+                                    </div>
+
+
+                                    <div className="form-group">
+                                        <label>Course Language</label>
+                                        <select
+                                            name='language'
+                                            value={courseLanguage}
+                                            onChange={(e) => setCourseLanguage(e.target.value)}
+                                        >
+                                            <option value="">Select</option>
+                                            <option value="english">English</option>
+                                            <option value="spanish">Spanish</option>
+                                            <option value="spanish">Hindi</option>
+                                            <option value="spanish">Gujarati</option>
+
+                                            <option value="other">Other</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div className='form-pair'>
+                                    <div className="form-group">
+                                        <label>Tags</label>
+                                        <input
+                                            name='tags'
+                                            type="text"
+                                            value={inputdata.tags}
+                                            onChange={handleChange}
+                                            placeholder="Enter Values Seperated by '' , ''"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Course Thumbnail</label>
+                                        <input
+                                            type="file"
+                                            name="thumbnail"
+                                            accept='.png'
+                                            // value={inputdata.thumbnail}
+                                            onChange={handleFileUpload}
+                                        />
+                                    </div>
+
+
+
+                                </div>
+                                <div className="form-group form-group-v">
+                                    <label>Long Description</label>
+                                    <input
+                                        type="text"
+                                        className="description"
+                                        name='courseDescriptionLong'
+                                        value={inputdata.courseDescriptionLong}
+                                        onChange={handleChange}
+                                        placeholder="Enter Your Course Description"
+                                    />
+
+                                </div>
+                                <button className='ccsubmit' onClick={handleSubmit}>Create Course</button>
+
                             </div>
-
-                            <div className="form-group">
-                                <label>long Description</label>
-                                <input
-                                    type="text"
-                                    className="description"
-                                    name='courseDescriptionLong'
-                                    value={inputdata.courseDescriptionLong}
-                                    onChange={handleChange}
-                                    placeholder="Enter Your Course Description"
-                                />
-
-                            </div>
-
-                            <div className="form-group">
-                                <label>Course Price (Rs)</label>
-                                <input
-                                    type="number"
-                                    value={inputdata.coursePrice}
-                                    onChange={handleChange}
-                                    placeholder="Enter Price (Enter '0' for free)"
-                                    name="coursePrice"
-                                />
-                            </div>
-
-                            <div className="form-group">
-                                <label>Course Level</label>
-                                <select
-                                    name='courseLevel'
-                                    value={courseLevel}
-                                    onChange={(e) => setCourseLevel(e.target.value)}
-                                >
-                                    <option value="">Select</option>
-                                    <option value="beginner">Beginner</option>
-                                    <option value="intermediate">Intermediate</option>
-                                    <option value="advanced">Advanced</option>
-                                </select>
-                            </div>
-
-                            <div className="form-group">
-                                <label>Course Language</label>
-                                <select
-                                    name='language'
-                                    value={courseLanguage}
-                                    onChange={(e) => setCourseLanguage(e.target.value)}
-                                >
-                                    <option value="">Select</option>
-                                    <option value="english">English</option>
-                                    <option value="spanish">Spanish</option>
-                                    <option value="spanish">Hindi</option>
-                                    <option value="spanish">Gujarati</option>
-
-                                    <option value="other">Other</option>
-                                </select>
-                            </div>
-
-                            <div className="form-group">
-                                <label>Prerequisites</label>
-                                <input
-                                    name='prerequisites'
-                                    type="text"
-                                    value={inputdata.prerequisites}
-                                    onChange={handleChange}
-                                    placeholder="Enter Values Seperated by '' , ''" 
-                                />
-                            </div>
-
-                            <div className="form-group">
-                                <label>Tags</label>
-                                <input
-                                    name='tags'
-                                    type="text"
-                                    value={inputdata.tags}
-                                    onChange={handleChange}
-                                    placeholder="Enter Values Seperated by '' , ''" 
-                                />
-                            </div>
-
-                            <div className="form-group">
-                                <label>Visibility</label>
-                                <select
-                                    name='visibility'
-                                    value={visibility}
-                                    onChange={(e) => setVisibility(e.target.value)}
-                                >
-                                    <option value="">Select</option>
-                                    <option value="Public">Public</option>
-                                    <option value="Protected">Protected</option>
-                                    <option value="Private">Private</option>
-                                </select>
-                            </div>
-                            <button className='ccsubmit' onClick={handleSubmit}>Create Course</button>
                         </div>
-                        {/* <div className=".create-course-image">
-                            <img src={image} alt="Profile" />
-                        </div> */}
+
+
+
                     </form>
                 </div >
+
             </div >
+
             <ToastContainer />
         </>
 
