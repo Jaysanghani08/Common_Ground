c85d27cae5ff941da70c7110206fbd35bcf6112f
Sahilmachhar <141858272+Sahilmachhar@users.noreply.github.com>
2023-11-25 21:36:23 +0530

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Error.css';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
-    <div>This is Error page</div>
-  )
-}
+    <div className="error-container">
+        <div className='er-num'>404</div>
+      <div className='er-headtag'>Oops! Something went wrong.</div>
+      {/* <p>We're sorry, but it seems like there was an error.</p> */}
+      <p className='er-content'>The page you are looking might have been removed had its name changed or is temporary unavailable.</p>
+      <Link to="/homepage">
+        <button className="er-home-button">Go Back to Home</button>
+      </Link>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
